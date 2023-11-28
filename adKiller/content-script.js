b9f649356767d8043b4c20e76f98636ef11b277c
TW <huangsanshui666@gmail.com>
2023-11-29 00:18:35 +0800

    去广告初始化

---
+++ adKiller/content-script.js
@@ -0,0 +1,11 @@
+﻿const removeAds = () => {
+  if (document.getElementById("removeAds")) {
+    return;
+  }
+  const temp = document.createElement("style");
+  temp.id = "removeAds";
+  (document.head || document.body).appendChild(temp);
+  const css = `divz{display:none !important}`;
+  temp.innerHTML = css;
+};
+removeAds();
--- readme.md
+++ readme.md
@@ -1,4 +1,7 @@
 chrome 浏览器插件
 
+adKiller:
+去广告
+
 coManager:
 收藏网址管理
